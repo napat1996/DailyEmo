59756bddbfb99385d6da1fe2c8f7c512ca412b1a
napat1996 <napat1996@gmail.com>
2018-08-29 19:12:57 +0700

    Add .gitignore

--- app/src/main/java/com/kmutt/android/dailyemo/LoginActivity/MainActivity.java
+++ app/src/main/java/com/kmutt/android/dailyemo/LoginActivity/MainActivity.java
@@ -21,12 +21,11 @@ import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final String TAG = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.e(TAG, "onCreate: Helloooooo" );
+
     }
 }
