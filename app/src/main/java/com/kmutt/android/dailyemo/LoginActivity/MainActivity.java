bbc3b7494ac184dc55a79bb0bf08a4e77454c86e
newptr <newnnew@hotmail.co.th>
2018-08-29 17:43:56 +0700

    test

--- app/src/main/java/com/kmutt/android/dailyemo/LoginActivity/MainActivity.java
+++ app/src/main/java/com/kmutt/android/dailyemo/LoginActivity/MainActivity.java
@@ -21,9 +21,12 @@ import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = "";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.e(TAG, "onCreate: Helloooooo" );
     }
 }
--- app/src/main/java/com/kmutt/android/dailyemo/LoginActivity/Test.java
+++
@@ -1,6 +0,0 @@
-package com.kmutt.android.dailyemo.LoginActivity;
-
-public class Test {
-
-}
-
