defa7681db785ebaad919466c3b2e79354a2dd9c
napat1996 <napat1996@gmail.com>
2018-08-31 19:11:50 +0700

    gson error, cannot find R

--- app/src/main/java/com/kmutt/android/dailyemo/fragments/ActivitiesFragment.java
+++ app/src/main/java/com/kmutt/android/dailyemo/fragments/ActivitiesFragment.java
@@ -1,4 +1,50 @@
 package com.kmutt.android.dailyemo.fragments;
 
-public class ActivitiesFragment extends InfoFragment{
+import android.content.Loader;
+import android.os.Bundle;
+
+import java.util.Date;
+
+public class ActivitiesFragment extends InfoFragment<DailyActivitySummary> {
+
+    @Override
+    public int getTitleResourceId() {
+        return R.string.activity_info;
+    }
+
+    @Override
+    protected int getLoaderId() {
+        return 3;
+    }
+
+    @Override
+    public Loader<ResourceLoaderResult<DailyActivitySummary>> onCreateLoader(int id, Bundle args) {
+        return ActivityService.getDailyActivitySummaryLoader(getActivity(), new Date());
+    }
+
+    @Override
+    public void onLoadFinished(Loader<ResourceLoaderResult<DailyActivitySummary>> loader, ResourceLoaderResult<DailyActivitySummary> data) {
+        super.onLoadFinished(loader, data);
+        if (data.isSuccessful()) {
+            bindActivityData(data.getResult());
+        }
+    }
+
+    public void bindActivityData(DailyActivitySummary dailyActivitySummary) {
+        StringBuilder stringBuilder = new StringBuilder();
+
+        Summary summary = dailyActivitySummary.getSummary();
+        Goals goals = dailyActivitySummary.getGoals();
+
+        stringBuilder.append("<b>TODAY</b> ");
+        stringBuilder.append("<br />");
+        printKeys(stringBuilder, summary);
+
+        stringBuilder.append("<br /><br />");
+        stringBuilder.append("<b>GOALS</b> ");
+        stringBuilder.append("<br />");
+        printKeys(stringBuilder, goals);
+
+        setMainText(stringBuilder.toString());
+    }
 }
